57516f555f7e2c51bc42476c451c3a79cca32877
Abhishek Tiwari <abhishektiwariisro1278@gmail.com>
2024-12-22 16:04:07 +0530

    adjust alignments in footer mobile view

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,21 +7,21 @@ import Bell from "../icons/bell";
 const Footer = () => {
   return (
     <div className="flex w-full justify-between text-[#a9b1d6] text-sm border-t-2 border-[#1b1e2e]">
-      <div className="flex justify-center items-center gap-2">
-        <div className="px-2 py-1 bg-[#3d59a1]">
+      <div className="flex justify-center items-center gap-1">
+        <div className="px-2 py-1 bg-[#3d59a1] hover:bg-[#282e44]">
           <Remote />
         </div>
-        <div className="flex justify-center items-center p-1 ">
+        <div className="flex justify-center items-center p-1 px-2 hover:bg-[#282e44]">
           <SourceControl height={18} width={18} />
           <span className="ml-1">main*</span>
         </div>
       </div>
-      <div className="flex px-4 justify-center items-center gap-5">
-        <div className="flex justify-center items-center px-1">
+      <div className="flex px-2 justify-center items-center gap-2">
+        <div className="flex justify-center items-center py-1 px-2 hover:bg-[#282e44]">
           <RadioTower />
           <span className="ml-1">Go Live</span>
         </div>
-        <div>
+        <div className="hover:bg-[#282e44] py-1 px-2">
           <Bell />
         </div>
       </div>

e6a5bfb2cb7763d49a303582d417ee6b26584cef
Abhishek Tiwari <abhishektiwariisro1278@gmail.com>
2025-01-13 20:09:30 +0530

    Fix:changed the image import removing public from it

--- src/components/home/Skill.tsx
+++ src/components/home/Skill.tsx
@@ -35,128 +35,127 @@ const skillsLogos = {
   ["Languages" as string]: [
     {
       name: "JavaScript",
-      image: "../../../public/logos/js-logo.png",
+      image: "../../../logos/js-logo.png",
     },
     {
       name: "TypeScript",
-      image: "../../../public/logos/ts-logo.png",
+      image: "../../../logos/ts-logo.png",
     },
     {
       name: "Python",
-      image: "../../../public/logos/python-logo.png",
+      image: "../../../logos/python-logo.png",
     },
     {
       name: "C#",
-      image: "../../../public/logos/csharp-logo.png",
+      image: "../../../logos/csharp-logo.png",
     },
   ],
   ["Front" as string]: [
     {
       name: "HTML5",
-      image: "../../../public/logos/html5-logo.png",
+      image: "../../../logos/html5-logo.png",
     },
     {
       name: "CSS3",
-      image: "../../../public/logos/css-logo.png",
+      image: "../../../logos/css-logo.png",
     },
     {
       name: "ReactJS",
-      image: "../../../public/logos/react-logo.png",
+      image: "../../../logos/react-logo.png",
     },
     {
       name: "NextJS",
-      image: "../../../public/logos/nextjs-logo.png",
+      image: "../../../logos/nextjs-logo.png",
     },
     {
       name: "Native",
-      image: "../../../public/logos/rnative-logo.png",
+      image: "../../../logos/rnative-logo.png",
     },
     {
       name: "TailwindCSS",
-      image: "../../../public/logos/tailwindcss-logo.jpg",
+      image: "../../../logos/tailwindcss-logo.jpg",
     },
     {
       name: "Shopify",
-      image: "../../../public/logos/shopify.jpg",
+      image: "../../../logos/shopify.jpg",
     },
     {
       name: "MaterialUI",
-      image: "../../../public/logos/materialui-logo.png",
+      image: "../../../logos/materialui-logo.png",
     },
     {
       name: "WordPress",
-      image: "../../../public/logos/wordpress.png",
+      image: "../../../logos/wordpress.png",
     },
   ],
   ["Back" as string]: [
     {
       name: "NodeJS",
-      image: "../../../public/logos/nodejs-logo.png",
+      image: "../../../logos/nodejs-logo.png",
     },
     {
       name: "ExpressJS",
-      image: "../../../public/logos/express-logo.png",
+      image: "../../../logos/express-logo.png",
     },
     {
       name: "NestJS",
-      image: "../../../public/logos/nestjs-logo.png",
+      image: "../../../logos/nestjs-logo.png",
     },
     {
       name: "MongoDB",
-      image: "../../../public/logos/mongodb-logo.webp",
+      image: "../../../logos/mongodb-logo.webp",
     },
     {
       name: "PostgresSql",
-      image: "../../../public/logos/postgres-logo.png",
+      image: "../../../logos/postgres-logo.png",
     },
     {
       name: "Firebase",
-      image: "../../../public/logos/firebase-logo.jpg",
+      image: "../../../logos/firebase-logo.jpg",
     },
     {
       name: "Heroku",
-      image: "../../../public/logos/heroku-logo.webp",
+      image: "../../../logos/heroku-logo.webp",
     },
     {
       name: "DigitalOcean",
-      image: "../../../public/logos/digital-ocean-logo.svg",
+      image: "../../../logos/digital-ocean-logo.svg",
     },
     {
       name: "awsS3",
-      image: "../../../public/logos/s3-logo.png",
+      image: "../../../logos/s3-logo.png",
     },
   ],
   ["Tools" as string]: [
     {
       name: "Git",
-      image: "../../../public/logos/git-logo.png",
+      image: "../../../logos/git-logo.png",
     },
     {
       name: "Github",
-      image: "../../../public/logos/github-logo.webp",
+      image: "../../../logos/github-logo.webp",
     },
-    { name: "Docker", image: "../../../public/logos/docker-logo.png" },
+    { name: "Docker", image: "../../../logos/docker-logo.png" },
     {
       name: "EsLint",
-      image: "../../../public/logos/eslint-logo.png",
+      image: "../../../logos/eslint-logo.png",
     },
     {
       name: "Redux",
-      image: "../../../public/logos/redux-logo.png",
+      image: "../../../logos/redux-logo.png",
     },
     {
       name: "Bitbucket",
-      image: "../../../public/logos/bitbucket.png",
+      image: "../../../logos/bitbucket.png",
     },
     {
       name: "Figma",
-      image: "../../../public/logos/figma-logo.webp",
+      image: "../../../logos/figma-logo.webp",
     },
     {
       name: "Mailchimp",
-      image: "../../../public/logos/mailchimp-logo.webp",
+      image: "../../../logos/mailchimp-logo.webp",
     },
-
   ],
 };
 
@@ -244,35 +243,35 @@ const Skill = () => {
               ))}
             </div>
           </div>
-            <div className="lg:grid grid-cols-3 hidden">
-              {skillsLogos[activeSkill].map((skill, index) => {
-                if (!skill.name)
-                  return <div key={index} className="h-[115px] w-24" />;
-                return (
-                  <FadeIn
-                    key={skill.name}
-                    className="h-[115px] w-24  place-self-center flex flex-col"
-                  >
-                    <div className="mt-auto ">
-                      <img
-                        src={skill.image}
-                        className="object-contain rounded-md m-auto"
-                        alt=""
-                        height={64}
-                        width={64}
-                        style={{
-                          width: 64,
-                          height: 64,
-                        }}
-                      />
-                      <h3 className="text-sm font-semibold tracking-tight text-[#525df3] text-center bg-white rounded-full w-min px-2 m-2 mx-auto">
-                        {skill.name}
-                      </h3>
-                    </div>
-                  </FadeIn>
-                );
-              })}
-            </div>
+          <div className="lg:grid grid-cols-3 hidden">
+            {skillsLogos[activeSkill].map((skill, index) => {
+              if (!skill.name)
+                return <div key={index} className="h-[115px] w-24" />;
+              return (
+                <FadeIn
+                  key={skill.name}
+                  className="h-[115px] w-24  place-self-center flex flex-col"
+                >
+                  <div className="mt-auto ">
+                    <img
+                      src={skill.image}
+                      className="object-contain rounded-md m-auto"
+                      alt=""
+                      height={64}
+                      width={64}
+                      style={{
+                        width: 64,
+                        height: 64,
+                      }}
+                    />
+                    <h3 className="text-sm font-semibold tracking-tight text-[#525df3] text-center bg-white rounded-full w-min px-2 m-2 mx-auto">
+                      {skill.name}
+                    </h3>
+                  </div>
+                </FadeIn>
+              );
+            })}
+          </div>
         </div>
         <div className="grid grid-cols-3 lg:hidden">
           {skillsLogos[activeSkill].map((skill, index) => {
